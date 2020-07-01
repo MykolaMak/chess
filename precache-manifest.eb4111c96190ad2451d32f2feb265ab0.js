6421e13140e79d785db17824fd77f024e60b39cd
MakolaMak <makolamak@gmail.com>
2020-07-01 16:56:36 +0300

    Updates

---
+++ precache-manifest.eb4111c96190ad2451d32f2feb265ab0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fabbb6e686b7803d300abbfa7f14f9cb",
+    "url": "/chess/index.html"
+  },
+  {
+    "revision": "d7dbdb0974d28c755986",
+    "url": "/chess/static/css/main.bcebed1f.chunk.css"
+  },
+  {
+    "revision": "8d1fd515475e2fdffd39",
+    "url": "/chess/static/js/2.7916e133.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chess/static/js/2.7916e133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7dbdb0974d28c755986",
+    "url": "/chess/static/js/main.6978ef05.chunk.js"
+  },
+  {
+    "revision": "fe54e1bce9cc9d1a3729",
+    "url": "/chess/static/js/runtime-main.bff3f106.js"
+  }
+]);
